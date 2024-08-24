dde28ef9890f779022b234bd9af79228d6303088
Eduard <eduard.daniel.toma@gmail.com>
2024-08-24 05:10:00 +0300

    added node server and Dockerfile's for UI/API

--- apps/task-api/src/main.ts
+++ apps/task-api/src/main.ts
@@ -1,1 +1,15 @@
-console.log('Hello World');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
+const app = express();
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+
+const tasks = [{id: 1}, {id: 2}, {id: 3}, {id: 4}];
+
+app.get('/url', (req, res, next) => {
+  res.json(tasks);
+});
+
+
